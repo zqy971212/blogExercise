b38e7fc631f3f8b4a2f66b51ba2e4f27bdf650b4
Qiyue-Chou <chinazhouqiyue@outlook.com>
2023-05-23 11:23:30 +0900

    Created Project

---
+++ src/main/java/pers/qy/blogexercise/BlogExerciseApplication.java
@@ -0,0 +1,13 @@
+package pers.qy.blogexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogExerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pers/qy/blogexercise/BlogExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.qy.blogexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
